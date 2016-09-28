c361bd204c2da0f4ba729dd4baac3bfd1b28e5b5
tithigupta <tithi@instamojo.com>
2016-09-28 18:26:58 +0530

    adding gunicorn conf

---
+++ guincorn.conf.py
@@ -0,0 +1,20 @@
+# Gunicorn Configuration File.
+# See: http://docs.gunicorn.org/en/latest/settings.html
+
+bind = '0.0.0.0:80'
+
+# Send error logs to stderr
+errorlog = '-'
+
+workers = 3
+threads = 1
+
+# Workers silent for more than this many seconds are killed and restarted.
+timeout = 300
+
+# Restart workers once in a while, to limit memory leaks, if any.
+max_requests = 300
+max_requests_jitter = 60
+
+# By preloading an application you can save some RAM resources as well as speed up server boot times.
+preload_app = True
