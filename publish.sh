7d314ada716a939075b5fa7de384dce73376e009
Ayushya Jaiswal <ayushya@instamojo.com>
2018-08-30 15:59:27 +0530

    Remove -f

--- publish.sh
+++ publish.sh
@@ -10,6 +10,6 @@ CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 DOCKER_TAG=${CURRENT_BRANCH//\//_}"-"$LATEST_COMMIT
 
 # Tag it with DOCKER_TAG
-docker tag -f html2pdf:latest $DOCKER_REGISTRY/html2pdf:$DOCKER_TAG
+docker tag html2pdf:latest $DOCKER_REGISTRY/html2pdf:$DOCKER_TAG
 
 docker push $DOCKER_REGISTRY/html2pdf:$DOCKER_TAG
